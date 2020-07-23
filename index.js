9c7599f2071fbf94dc8af2e4624371b5cfaf690a
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-07-23 21:28:59 +0530

    part 2

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if (isDevelopment) {
 	// production
 	app.use(cors());
 } else {
-	app.use(cors({ origin: CLIENT_URL, optionsSuccessStatus: 200 }));
+	app.use(cors({ origin: process.env.CLIENT_URL, optionsSuccessStatus: 200 }));
 }
 
 app.use("/api", notesRoutes);
