bffc7fd544a28eace88edb41733ce8c625dea5ea
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-07-24 15:14:32 +0530

    complete change in client

--- index.js
+++ index.js
@@ -33,18 +33,18 @@ if (isDevelopment) {
 
 app.use("/api", notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	// Serve any static files
-	app.use(express.static(path.join(__dirname, "client/build")));
-	// Handle React routing, return all requests to React app
-	app.get("*", function (req, res) {
-		res.sendFile(path.join(__dirname, "client/build", "index.html"));
-	});
-}
+// if (process.env.NODE_ENV === "production") {
+// 	// Serve any static files
+// 	app.use(express.static(path.join(__dirname, "client/build")));
+// 	// Handle React routing, return all requests to React app
+// 	app.get("*", function (req, res) {
+// 		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// 	});
+// }
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname + "/client/build/index.html"));
-// });
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 mongoose
 	.connect(process.env.DATABASE_URL, {
